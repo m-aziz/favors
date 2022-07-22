5e3ab1185a8cc078826bee4bb0f1591f3891660c
Muhammad Aziz <ma5264@nyu.edu>
2022-07-22 17:51:44 -0400

    added tailwind and landing page skeleton

---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,11 @@
+
+
+function NavBar() {
+    return (
+        <Card shape="rounded" bgColour="white" borderColour="slate-20" shadow="soft">
+            My awesome card
+        </Card>
+    )
+}
+
+export default NavBar;
